db295c799532ce12a408eb86b2ba4632bc6491bf
Dennis Schulmeister-Zimolong <dennis@patk.org>
2023-11-19 21:39:18 +0100

    Publish-Skript: Alte Serveradresse

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,5 +3,5 @@ set -e
 
 npm run build
 #rsync -rvz build/ dennis@pingu-mobil.de:/srv/www/www.wpvs.de/repo/iot-2023/folien
-# rsync -rvz build/ dennis@vhermes:/srv/www/www.wpvs.de/repo/iot-2023/folien
-rsync -rvz -e 'ssh -p 4444' build/ dennis@wikiberd.de:/srv/www/www.wpvs.de/repo/iot-2023/folien
+rsync -rvz build/ dennis@vhermes:/srv/www/www.wpvs.de/repo/iot-2023/folien
+#rsync -rvz -e 'ssh -p 4444' build/ dennis@wikiberd.de:/srv/www/www.wpvs.de/repo/iot-2023/folien
